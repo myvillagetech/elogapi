420c93d6daac9f920200865eafa4bdf798681978
J Prashanth Kumar <83943164+prashanthj32@users.noreply.github.com>
2022-12-12 20:42:47 +0530

    updated genric api's

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { OrganizationTypeModule } from './auth/generic/organization-type/organization-type.module';
+import { OrganizationTypeModule } from './generic/organization-type/organization-type.module';
 import { MONGO_KEYS } from './config/keys.config';
 import { OrganizationsModule } from './organizations/organizations.module';
 import { UsersModule } from './users/users.module';
