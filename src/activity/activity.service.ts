f74624cc1439736d5ead400d517e137bc11f7c39
Hari <hari.katakam@simplifyvms.com>
2023-01-11 11:28:29 +0530

    update prettier and es list settings

--- src/activity/activity.service.ts
+++ src/activity/activity.service.ts
@@ -4,23 +4,31 @@ import { Model, Types } from 'mongoose';
 import { MODEL_ENUMS } from 'src/shared/enums/model.enums';
 import { ActivityLogDto } from './dto/activity-log.dto';
 import { ActivityDto } from './dto/activity.dto';
-import { ArchiveActivityDto, UpdateActivityDto, UpdateActivityDueDateDto, UpdateActivityStatusDto } from './dto/update-activity.dto';
+import {
+    ArchiveActivityDto,
+    UpdateActivityDto,
+    UpdateActivityDueDateDto,
+    UpdateActivityStatusDto,
+} from './dto/update-activity.dto';
 import { ActivityDocument } from './schemas/activity.schema';
 
 @Injectable()
 export class ActivityService {
-
-    @InjectModel(MODEL_ENUMS.ACTIVITIES) private activityModel: Model<ActivityDocument>
-    constructor() {
-
-    }
+    @InjectModel(MODEL_ENUMS.ACTIVITIES)
+    private activityModel: Model<ActivityDocument>;
+    constructor() {}
 
     async createActivity(activityDto: ActivityDto): Promise<ActivityDocument> {
-        let toDay = new Date()
-        const dueDate = new Date(toDay.setDate(toDay.getDate()+21));
-        const newActivity = await new this.activityModel({...activityDto,dueDate : dueDate,dueDateLog : {dueDate : dueDate}, assignTo : activityDto.organization[0] });
+        let toDay = new Date();
+        const dueDate = new Date(toDay.setDate(toDay.getDate() + 21));
+        const newActivity = await new this.activityModel({
+            ...activityDto,
+            dueDate: dueDate,
+            dueDateLog: { dueDate: dueDate },
+            assignTo: activityDto.organization[0],
+        });
         // newActivity.markModified('attachments');
-        return newActivity.save()
+        return newActivity.save();
     }
 
     async getAllActivities(): Promise<ActivityDocument[]> {
@@ -58,28 +66,34 @@ export class ActivityService {
                     foreignField: '_id',
                     as: 'userData',
                     pipeline: [{ $project: { password: 0 } }],
-                }
+                },
             },
             {
-                $match: { _id: new Types.ObjectId(activityId) }
-            }
-        ])
+                $match: { _id: new Types.ObjectId(activityId) },
+            },
+        ]);
 
         if (!activity) {
             throw new NotFoundException('Activity data not found');
         }
 
-        return activity
+        return activity;
     }
 
-    async updateActivityByActivityId(activityId: string, activityData: UpdateActivityDto): Promise<ActivityDocument> {
-        const updatedActivity = await this.activityModel.findByIdAndUpdate(activityId, activityData);
+    async updateActivityByActivityId(
+        activityId: string,
+        activityData: UpdateActivityDto,
+    ): Promise<ActivityDocument> {
+        const updatedActivity = await this.activityModel.findByIdAndUpdate(
+            activityId,
+            activityData,
+        );
 
         if (!updatedActivity) {
             throw new NotFoundException('Activity data not found');
         }
 
-        return updatedActivity
+        return updatedActivity;
     }
 
     async deleteActivityByActivityId(activityId: string): Promise<any> {
@@ -89,40 +103,16 @@ export class ActivityService {
             throw new NotFoundException('Activity data not found');
         }
 
-        return result
+        return result;
     }
 
-    async archiveActivity(activityId: string, activityDetails: ArchiveActivityDto): Promise<any> {
-        const result = await this.activityModel.findByIdAndUpdate(activityId, activityDetails);
-
-        if (!result) {
-            throw new NotFoundException('Activity data not found');
-        }
-
-        return result
-    }
-
-    async updateActivityLogByActivityId(activityId: string, activityLog: ActivityLogDto): Promise<any> {
-
-        const queryObject = { $push: { "activityLog": {...activityLog, userId: new Types.ObjectId(activityLog.userId)} } };
-
-        if(activityLog.visibility){
-            queryObject["visibility"] = activityLog.visibility
-        }
-
-        if (activityLog.priority) {
-            queryObject["priority"] = activityLog.priority
-        }
-
-        if (activityLog.status) {
-            queryObject["status"] = activityLog.status;
-            queryObject.$push["statusLog"] = {status: activityLog.status}
-        }
-
-        const result = await this.activityModel.updateOne(
-            { "_id": new Types.ObjectId(activityId) },
-            queryObject
-
+    async archiveActivity(
+        activityId: string,
+        activityDetails: ArchiveActivityDto,
+    ): Promise<any> {
+        const result = await this.activityModel.findByIdAndUpdate(
+            activityId,
+            activityDetails,
         );
 
         if (!result) {
@@ -132,20 +122,62 @@ export class ActivityService {
         return result;
     }
 
-    async updateActivityStatus(activityId : string , activityStatus : UpdateActivityStatusDto):Promise<any>{
+    async updateActivityLogByActivityId(
+        activityId: string,
+        activityLog: ActivityLogDto,
+    ): Promise<any> {
+        const queryObject = {
+            $push: {
+                activityLog: {
+                    ...activityLog,
+                    userId: new Types.ObjectId(activityLog.userId),
+                },
+            },
+        };
 
-        const result = await this.activityModel.findByIdAndUpdate(activityId,activityStatus, { new: true });
+        if (activityLog.visibility) {
+            queryObject['visibility'] = activityLog.visibility;
+        }
+
+        if (activityLog.priority) {
+            queryObject['priority'] = activityLog.priority;
+        }
+
+        if (activityLog.status) {
+            queryObject['status'] = activityLog.status;
+            queryObject.$push['statusLog'] = { status: activityLog.status };
+        }
+
+        const result = await this.activityModel.updateOne(
+            { _id: new Types.ObjectId(activityId) },
+            queryObject,
+        );
 
         if (!result) {
             throw new NotFoundException('Activity data not found');
         }
 
         return result;
-
     }
 
-    async getAllArchiveActivities():Promise<ActivityDocument[]>{
+    async updateActivityStatus(
+        activityId: string,
+        activityStatus: UpdateActivityStatusDto,
+    ): Promise<any> {
+        const result = await this.activityModel.findByIdAndUpdate(
+            activityId,
+            activityStatus,
+            { new: true },
+        );
 
+        if (!result) {
+            throw new NotFoundException('Activity data not found');
+        }
+
+        return result;
+    }
+
+    async getAllArchiveActivities(): Promise<ActivityDocument[]> {
         const results = await this.activityModel.aggregate([
             {
                 $lookup: {
@@ -156,9 +188,9 @@ export class ActivityService {
                 },
             },
             {
-                $match: { isArchive : true}
-            }
-        ])
+                $match: { isArchive: true },
+            },
+        ]);
 
         if (!results) {
             throw new NotFoundException('Activity data not found');
@@ -167,26 +199,23 @@ export class ActivityService {
         return results;
     }
 
-    async updateActivityDuedate(activityId : string, dueDateDetails : UpdateActivityDueDateDto):Promise<any>{
-
+    async updateActivityDuedate(
+        activityId: string,
+        dueDateDetails: UpdateActivityDueDateDto,
+    ): Promise<any> {
         const result = await this.activityModel.updateOne(
-            { "_id": new Types.ObjectId(activityId) },
+            { _id: new Types.ObjectId(activityId) },
             {
-                dueDate : dueDateDetails.dueDate,
-                $push : {dueDateLog : dueDateDetails}
-            }
+                dueDate: dueDateDetails.dueDate,
+                $push: { dueDateLog: dueDateDetails },
+            },
         );
         if (!result) {
             throw new NotFoundException('Activity data not found');
         }
 
         return result;
-
     }
 
-
-
-    async activitySerachCriteria(){
-
-    }
+    async activitySerachCriteria() {}
 }
