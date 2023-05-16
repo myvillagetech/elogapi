c16e7431e1f191b96aafd285de77b167ef038288
J Prashanth Kumar <83943164+prashanthj32@users.noreply.github.com>
2023-05-16 16:16:29 +0530

    added static files

--- src/config/keys.config.ts
+++ src/config/keys.config.ts
@@ -1,3 +1,3 @@
 export const MONGO_KEYS = {
-    url : `mongodb://elog:elog1234@ac-c2udmx1-shard-00-00.a4txoie.mongodb.net:27017,ac-c2udmx1-shard-00-01.a4txoie.mongodb.net:27017,ac-c2udmx1-shard-00-02.a4txoie.mongodb.net:27017/?ssl=true&replicaSet=atlas-k4xs4m-shard-0&authSource=admin&retryWrites=true&w=majority`
-}
+    url: `mongodb://elog:elog1234@ac-c2udmx1-shard-00-00.a4txoie.mongodb.net:27017,ac-c2udmx1-shard-00-01.a4txoie.mongodb.net:27017,ac-c2udmx1-shard-00-02.a4txoie.mongodb.net:27017/scrum?ssl=true&replicaSet=atlas-k4xs4m-shard-0&authSource=admin&retryWrites=true&w=majority`,
+};
