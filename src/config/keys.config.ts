925b9d276708ea79ac92d856fd6b4895d63c3e81
J Prashanth Kumar <83943164+prashanthj32@users.noreply.github.com>
2023-05-27 19:09:25 +0530

    added dump

--- src/config/keys.config.ts
+++ src/config/keys.config.ts
@@ -1,3 +1,8 @@
+// export const MONGO_KEYS = {
+//     url: `mongodb://elog:elog1234@ac-c2udmx1-shard-00-00.a4txoie.mongodb.net:27017,ac-c2udmx1-shard-00-01.a4txoie.mongodb.net:27017,ac-c2udmx1-shard-00-02.a4txoie.mongodb.net:27017/scrum?ssl=true&replicaSet=atlas-k4xs`,
+// };
+
+
 export const MONGO_KEYS = {
     url: `mongodb://elog:elog1234@ac-c2udmx1-shard-00-00.a4txoie.mongodb.net:27017,ac-c2udmx1-shard-00-01.a4txoie.mongodb.net:27017,ac-c2udmx1-shard-00-02.a4txoie.mongodb.net:27017/scrum?ssl=true&replicaSet=atlas-k4xs4m-shard-0&authSource=admin&retryWrites=true&w=majority`,
 };
